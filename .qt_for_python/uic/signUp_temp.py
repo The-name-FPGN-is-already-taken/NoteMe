d4e512b5b8d51ffec8e7313ab120d72b9ffaa52f
Chawapon <68158123+nutt07@users.noreply.github.com>
2021-12-19 00:38:52 +0700

    Revert "Revert 28 parn branch2.1"

--- .qt_for_python/uic/signUp_temp.py
+++ .qt_for_python/uic/signUp_temp.py
@@ -12,7 +12,7 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-import loginWindowSource_rc
+
 
 class Ui_loginWindowDialog(object):
     def setupUi(self, loginWindowDialog):
